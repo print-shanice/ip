b36ad23170e962704fdf6a5241f87a1d6897914f
shaba1l <shanicelaijiaxin2009@gmail.com>
2026-01-23 00:24:08 +0800

    Added I/O Testing

--- src/main/java/kiki.java
+++ src/main/java/kiki.java
@@ -7,21 +7,23 @@ public class kiki {
         int numOfTasks = 0;
 
 
-        System.out.println(" ____________________________________________________________\n" +
-                " good morning \n" +
-                " i'm kiki\n" +
-                " what do you want?\n" +
-                " ____________________________________________________________\n");
+        System.out.println(" ____________________________________________________________");
+        System.out.println(" good morning");
+        System.out.println(" i'm kiki");
+        System.out.println(" what do you want?");
+        System.out.println(" ____________________________________________________________");
+
 
         while (true) {
             String input = scanner.nextLine();
             if (input.equalsIgnoreCase("bye")) {
                 System.out.println(" ____________________________________________________________");
-                System.out.println(" finallyyyyy, bye!\n" + " ____________________________________________________________");
+                System.out.println(" finallyyyyy, bye!");
+                System.out.println(" ____________________________________________________________");
                 break;
             }
             else if (input.equalsIgnoreCase("list")) {
-                System.out.println(" ____________________________________________________________\n");
+                System.out.println(" ____________________________________________________________");
                 if (numOfTasks == 0) {
                     System.out.println(" no tasks added");
                 } else {
@@ -29,13 +31,13 @@ public class kiki {
                         System.out.println(" " + (i + 1) + ". " + tasks[i]);
                     }
                 }
-                System.out.println(" ____________________________________________________________\n");
+                System.out.println(" ____________________________________________________________");
             }
             else if (input.toLowerCase().startsWith("mark")) {
                 int index = Integer.parseInt(input.split(" ")[1]) - 1;
                 tasks[index].markDone();
                 System.out.println(" ____________________________________________________________");
-                System.out.println("you're welcome, i've marked this task as done: ");
+                System.out.println("you're welcome, i've marked this task as done:");
                 System.out.println(" " + tasks[index]);
                 System.out.println(" ____________________________________________________________");
             }
