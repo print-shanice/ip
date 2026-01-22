9edfe5afc1757b3c5ae75f8a5a2c7572a8ba50b0
shaba1l <shanicelaijiaxin2009@gmail.com>
2026-01-22 19:32:36 +0800

    Implemented tasks

--- src/main/java/kiki.java
+++ src/main/java/kiki.java
@@ -3,8 +3,12 @@ import java.util.Scanner;
 public class kiki {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String[] tasks = new String[100];
+        int numOfTasks = 0;
+
+
         System.out.println(" ____________________________________________________________\n" +
-                " hi! \n" +
+                " ughhhhhh hi \n" +
                 " i'm kiki\n" +
                 " what do you want?\n" +
                 " ____________________________________________________________\n");
@@ -12,12 +16,27 @@ public class kiki {
         while (true) {
             String input = scanner.nextLine();
             if (input.equalsIgnoreCase("bye")) {
+                System.out.println(" ____________________________________________________________");
                 System.out.println(" finallyyyyy, bye!\n" + " ____________________________________________________________");
                 break;
             }
+            else if (input.equalsIgnoreCase("list")) {
+                System.out.println(" ____________________________________________________________\n");
+                if (numOfTasks == 0) {
+                    System.out.println(" no tasks added");
+                } else {
+                    for (int i = 0; i < numOfTasks; i++) {
+                        System.out.println(" " + (i + 1) + ". " + tasks[i]);
+                    }
+                }
+                System.out.println(" ____________________________________________________________\n");
+            }
             else {
-                System.out.println(" ____________________________________________________________\n " +
-                        input + "\n ____________________________________________________________");
+                tasks[numOfTasks] = input;
+                numOfTasks++;
+                System.out.println(" ____________________________________________________________");
+                System.out.println(" added: " + input);
+                System.out.println(" ____________________________________________________________");
             }
         }
         scanner.close();
