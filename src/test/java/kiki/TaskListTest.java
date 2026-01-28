bea01bf4950e850b849bb4da22f53cc2270460d9
shaba1l <shanicelaijiaxin2009@gmail.com>
2026-01-28 20:19:41 +0800

    Implement JUnit Tests

---
+++ src/test/java/kiki/TaskListTest.java
@@ -0,0 +1,28 @@
+package kiki;
+
+import task.Task;
+import task.ToDo;
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TaskListTest {
+
+    @Test
+    public void addTask_valid_taskAddedSuccessfully() {
+        TaskList list = new TaskList(new ArrayList<>());
+        Task todo = new ToDo("eat");
+        list.addTask(todo);
+        assertEquals(1, list.size());
+        assertEquals(todo, list.getTask(0));
+    }
+
+    @Test
+    public void deleteTask_invalidIndex_throwsException() {
+        TaskList list = new TaskList(new ArrayList<>());
+        assertThrows(IndexOutOfBoundsException.class, () -> {
+            list.removeTask(0);
+        });
+    }
+}
---
+++ src/test/java/task/DeadlineTest.java
@@ -0,0 +1,22 @@
+package task;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class DeadlineTest {
+
+    @Test
+    public void constructor_invalidDate_throwsException() {
+        IllegalArgumentException e = assertThrows(IllegalArgumentException.class, () -> new Deadline("homework", "2019-12-02"));
+        assertEquals("date must be in format D/M/YYYY HHmm", e.getMessage());
+    }
+
+    @Test
+    public void constructor_valid_success() {
+        Deadline d = new Deadline("homework", "2/12/2019 1800");
+        String result = d.toString();
+        assertTrue(result.contains("homework"));
+        assertTrue(result.contains("Dec"));
+    }
+}
